d2d3ea93e493a6f2d0d1deef422429f1ff940119
EmanuelGF <emanuelfaisca@gmail.com>
2021-07-08 13:43:01 +0100

    Adicionados ficheiro de dadosPT Adicionados testes valPT

--- validatuga.js
+++ validatuga.js
@@ -2,11 +2,21 @@ const Validatuga = {
 
   //Validações comuns
   comuns: {
+    /**
+     * Verifica se é um endereço de email válido.
+     * @param {string} email 
+     * @returns {boolean}
+     */
     valEmail: function (email) {
         const padrao = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/ //regex
         return padrao.test(email.trim());
     },
     
+    /**
+     * Verifica se o input é composto apenas por caracteres a-z e A-Z.
+     * @param {string} input 
+     * @returns {boolean} 
+     */
     soLetras: function (input) {
         const padrao = /^[a-zA-Z]*$/
         return padrao.test(input.trim())
@@ -19,16 +29,19 @@ const Validatuga = {
    * de telefone, CC, telemovel, NIF etc... 
    */
   valPT: {
-    telefoneFixo: function () {
+    
+    /**
+     * Valida telefone fixo
+     * @param {number} num numero de telefone fixo.
+     * @returns {boolean} true se for um numero de telefone fixo valido.
+     */
+    telefoneFixo: function (num) {
         return null
     },
     telemovel: function () {
         return null
     },
-    telemovelVF: function () {
-        return null
-    },
-    telemovelMEO: function () {
+    telemovelOperadora: function () {
         return null
     },
     codPostalVal: function () {
