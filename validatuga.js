255c5e1aa27ff72cb302492f9b5703e162f31e2d
EmanuelGF <emanuelfaisca@gmail.com>
2021-07-08 15:15:16 +0100

    Update readme. dataPT.js agora é data.js para incluir dados de todos os paises de lingua portuguesa.

--- validatuga.js
+++ validatuga.js
@@ -1,68 +1,74 @@
 const Validatuga = {
-
   //Validações comuns
   comuns: {
     /**
      * Verifica se é um endereço de email válido.
-     * @param {string} email 
+     * @param {string} email
      * @returns {boolean}
      */
     valEmail: function (email) {
-        const padrao = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/ //regex
-        return padrao.test(email.trim());
+      const padrao = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/; //regex
+      return padrao.test(email.trim());
     },
-    
+
     /**
      * Verifica se o input é composto apenas por caracteres a-z e A-Z.
-     * @param {string} input 
-     * @returns {boolean} 
+     * @param {string} input
+     * @returns {boolean}
      */
     soLetras: function (input) {
-        const padrao = /^[a-zA-Z]*$/
-        return padrao.test(input.trim())
-    }
-    
+      const padrao = /^[a-zA-Z]*$/;
+      return padrao.test(input.trim());
+    },
   },
 
   /**
    * Validações de dados de Portugal como numeros
-   * de telefone, CC, telemovel, NIF etc... 
+   * de telefone, CC, telemovel, NIF etc...
    */
   valPT: {
-    
     /**
      * Valida telefone fixo
      * @param {number} num numero de telefone fixo.
      * @returns {boolean} true se for um numero de telefone fixo valido.
      */
     telefoneFixo: function (num) {
-        return null
+      return null;
     },
     telemovel: function () {
-        return null
+      return null;
     },
     telemovelOperadora: function () {
-        return null
+      return null;
     },
     codPostalVal: function () {
-        return null
+      return null;
     },
     codPostalNome: function () {
-        return null
+      return null;
     },
     ccVal: function () {
-        return null
+      return null;
     },
     nif: function () {
-        return null
+      return null;
     },
     nss: function () {
-        return null
+      return null;
     },
-  }
-  
+  },
 
+  /**
+   * Validações de dados sociais do Brazil como numeros
+   * de telefone, CC, telemovel, NIF etc...
+   */
+  valBR: {},
 
+  /**
+   * Validações de dados de Angola como numeros
+   * de telefone, CC, telemovel, NIF etc...
+   */
+  valANG: {},
 };
 
 module.exports = Validatuga;
